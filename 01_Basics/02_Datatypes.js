4dfef0f7e5389b0991ff2e15df362a0d9da7f718
Samyak Jain <samyak.9109@gmail.com>
2025-09-30 12:04:54 +0000

    Learning DataTypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as new version of Js
+
+//alert(3 + 3) 
+/*
+gives error cause it have different syntax in node , 
+this is mostly used previoluty in this form when Js used to run of browser 
+*/
+
+console.log (3 
+    + 
+    3); console.log("Samyak");//Bad coding practice , reudces code readibilty
+
+// DataTypes
+
+let name = "Samyak" // String DataType in double quotes
+let age  = 21 // number 
+let isLoggedIn = false // boolean type, true or false
+
+//null = standalone value
+//undefined = not defined
+//symbol = unique
+
+//object 
+
+console.log(typeof name);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
