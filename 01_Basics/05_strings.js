d8b7774b1f4f65e79044805f5b2a78da65e624eb
Samyak Jain <samyak.9109@gmail.com>
2025-10-07 13:18:12 +0000

    JS Strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,9 +1,11 @@
 const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value"); // Old method for string concatenation 
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//uses String interColation using backticks
+//${} wrtie variable in the curly braces
 
 const gameName = new String('hitesh-hc-com')
 
@@ -11,12 +13,12 @@ const gameName = new String('hitesh-hc-com')
 // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4)//outer boound is exclusive , mean 4 will not be included
 console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
@@ -24,7 +26,7 @@ console.log(anotherString);
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());//trims leading and trailing white spaces
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
@@ -33,3 +35,4 @@ console.log(url.replace('%20', '-'))
 console.log(url.includes('sundar'))
 
 console.log(gameName.split('-'));
+//split method splits strings based on the character given , like in out case the getname is split
