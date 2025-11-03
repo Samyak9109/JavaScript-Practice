d3743e4c154f63517514cab8a4c9847da588f2f4
samyak <samyak.9109@gmail.com>
2025-11-03 16:57:09 +0530

    Objects in JS

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,15 +1,17 @@
-// singleton
-// Object.create
+// singleton is an object which can have only one instance
+// Objects in JavaScript are used to store keyed collections of various data and more complex entities.
+// Objects can be declared as literals and as constructors
 
-// object literals
+//objects as literals are single instances and are useful for storing data as key-value pairs
 
+//declaring an object using symbol as key
 const mySym = Symbol("key1")
 
-
+//declaring an object using object literal
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1",//using symbol as key but it will not be shown in console.log cause symbol properties are not enumerable
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -17,22 +19,38 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+//Accessing object properties
+console.log(JsUser.email) 
+console.log(JsUser["email"])//bracket notation is useful when key has space or special character
+console.log(JsUser["full name"])//it is given in double quotes because of space
+console.log(JsUser[mySym])// accessing symbol key using square bracket notation cause dot notation won't work
 
+
+//Updating object properties
 JsUser.email = "hitesh@chatgpt.com"
 // Object.freeze(JsUser)
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
-JsUser.greeting = function(){
+//Adding new property to object
+JsUser.phoneNumber = "9999999999"
+// console.log(JsUser);
+
+
+//adding function in object
+JsUser.greeting = function(){//method inside object
     console.log("Hello JS user");
 }
-JsUser.greetingTwo = function(){
+JsUser.greetingTwo = function(){//string interpolation using backticks
     console.log(`Hello JS user, ${this.name}`);
 }
 
+
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+//Creating object using constructor
+const UserTwo = new Object()
+UserTwo.name = "Hitesh"
+UserTwo.age = 18
+UserTwo.location = "Jaipur"
