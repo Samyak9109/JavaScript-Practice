b2183d0d7128100ee3db537a798c850386462ba6
samyak <samyak.9109@gmail.com>
2025-11-07 17:16:38 +0530

    Leanring Objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,7 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+//using Objects as Singleton
+
+// const tinderUser = new Object() //singletion object creation syntax
+const tinderUser = {} // object literal syntax , non singleton
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -23,14 +25,14 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 } // gives object inside object
+// const obj3 = Object.assign({}, obj1, obj2, obj4)// to merge multiple objects
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2} // spread operator to merge multiple objects
 // console.log(obj3);
 
 
-const users = [
+const users = [ // array of objects
     {
         id: 1,
         email: "h@gmail.com"
@@ -45,17 +47,17 @@ const users = [
     },
 ]
 
-users[1].email
+users[1].email // to access array of objects
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser)); // to get keys of object as array
+// console.log(Object.values(tinderUser)); // to get values of object as array
+// console.log(Object.entries(tinderUser)); // to get key-value pairs of object as array
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // to check if object has specific property
 
 
-const course = {
+const course = { // object literal
     coursename: "js in hindi",
     price: "999",
     courseInstructor: "hitesh"
@@ -63,19 +65,19 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+const {courseInstructor: instructor} = course // object destructuring with alias
 
 // console.log(courseInstructor);
 console.log(instructor);
 
-// {
+// {// object literal shorthand
 //     "name": "hitesh",
 //     "coursename": "js in hindi",
 //     "price": "free"
 // }
 
-[
-    {},
+[ // array of empty objects
+    {}, // empty object
     {},
     {}
 ]
