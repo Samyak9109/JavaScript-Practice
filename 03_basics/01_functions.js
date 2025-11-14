4c6368591a3b094fe3199cf38b191725e24cdf2d
samyak <samyak.9109@gmail.com>
2025-11-14 15:53:27 +0530

    Learning Funtions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,5 +1,5 @@
 
-function sayMyName(){
+function sayMyName(){ // function declaration 
     console.log("H");
     console.log("I");
     console.log("T");
@@ -8,7 +8,7 @@ function sayMyName(){
     console.log("H");
 }
 
-// sayMyName()
+// sayMyName() // referencing / calling / invoking the function
 
 // function addTwoNumbers(number1, number2){
 
@@ -19,38 +19,38 @@ function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
     // return result
-    return number1 + number2
+    return number1 + number2// return keyword - used to return some value from the function
 }
 
 const result = addTwoNumbers(3, 5)
 
-// console.log("Result: ", result);
+// console.log("Result: ", result); 
 
 
-function loginUserMessage(username = "sam"){
+function loginUserMessage(username = "sam"){ // function with default parameter
     if(!username){
         console.log("PLease enter a username");
         return
     }
-    return `${username} just logged in`
+    return `${username} just logged in` // template string
 }
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
 
 
-function calculateCartPrice(val1, val2, ...num1){
-    return num1
+function calculateCartPrice(val1, val2, ...num1){ // rest operator
+    return num1 // num1 is an array of all the values passed after val2
 }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
-const user = {
+const user = { // object
     username: "hitesh",
     prices: 199
 }
 
-function handleObject(anyobject){
+function handleObject(anyobject){ // Funtion accepting object as parameter as a generic name
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
@@ -62,7 +62,7 @@ handleObject({
 
 const myNewArray = [200, 400, 100, 600]
 
-function returnSecondValue(getArray){
+function returnSecondValue(getArray){ // giving generic name to parameter
     return getArray[1]
 }
 
